327c4612f8efe17a15ddc8a61ad249642e1c42ff
Filip Karaś <filip2801@gmail.com>
2021-04-29 17:31:43 +0200

    Add indexes

---
+++ src/main/resources/db/migration/V3__add_indexes.sql
@@ -0,0 +1,4 @@
+create index idx_type on audit_log(type);
+create index idx_subtype on audit_log(subtype);
+create index idx_date on audit_log(date);
+create index idx_username on audit_log(username);
